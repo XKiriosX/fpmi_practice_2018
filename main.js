9ae3a36373333c1b77b1a6422f75fe0e6ebc2fd1
Daniil Bondarau <Slayerdog4@gmail.com>
2018-04-14 21:04:13 +0300

    added basic functions

--- main.js
+++ main.js
@@ -1,74 +1,83 @@
-(function () {
-    let posts = [
-        {
-            id: '1',
-            author: 'USER228',
-            date: new Date(),
-            photoLink: 'src/post.jpg',
-            hashtags: ['#omagad', '#lol', '#wtf']
-        },
-        {
-            id: '2',
-            author: 'EscobaRrR',
-            date: new Date(),
-            photoLink: 'src/post.jpg',
-            hashtags: ['#eto', '#tagi']
-        },
-        {
-            id: '3',
-            author: 'Pablo228',
-            date: new Date(),
-            photoLink: 'src/post.jpg',
-            hashtags: ['#omagad', '#lol']
-        },
-        {
-            id: '4',
-            author: 'Pablo228',
-            date: new Date(),
-            photoLink: 'src/post.jpg',
-            hashtags: ['#omagad', '#lol']
-        },
-        {
-            id: '5',
-            author: 'Pablo228',
-            date: new Date(),
-            photoLink: 'src/post.jpg',
-            hashtags: ['#omagad', '#lol']
-        },
-        {
-            id: '6',
-            author: 'Pablo228',
-            date: new Date(),
-            photoLink: 'src/post.jpg',
-            hashtags: ['#omagad', '#lol']
-        },
-        {
-            id: '7',
-            author: 'Pablo228',
-            date: new Date(),
-            photoLink: 'src/post.jpg',
-            hashtags: ['#omagad', '#lol']
-        },
-        {
-            id: '8',
-            author: 'Pablo228',
-            date: new Date(),
-            photoLink: 'src/Hlib-Paper.jpg',
-            hashtags: ['#omagad', '#lol']
-        },
-        {
-            id: '9',
-            author: 'F1latovv',
-            date: new Date(),
-            photoLink: 'src/Nikita-Max.jpg',
-            hashtags: ['#omagad', '#lol', '#nice_roof']
-        }
-    ];
+let posts = [
+    {
+        id: '1',
+        author: 'USER228',
+        date: new Date(),
+        photoLink: 'src/post.jpg',
+        hashtags: ['#omagad', '#lol', '#wtf']
+    },
+    {
+        id: '2',
+        author: 'EscobaRrR',
+        date: new Date(),
+        photoLink: 'src/post.jpg',
+        hashtags: ['#eto', '#tagi']
+    },
+    {
+        id: '3',
+        author: 'Pablo228',
+        date: new Date(),
+        photoLink: 'src/post.jpg',
+        hashtags: ['#omagad', '#lol']
+    },
+    {
+        id: '4',
+        author: 'Pablo228',
+        date: new Date(),
+        photoLink: 'src/post.jpg',
+        hashtags: ['#omagad', '#lol']
+    },
+    {
+        id: '5',
+        author: 'Pablo228',
+        date: new Date(),
+        photoLink: 'src/post.jpg',
+        hashtags: ['#omagad', '#lol']
+    },
+    {
+        id: '6',
+        author: 'Pablo228',
+        date: new Date(),
+        photoLink: 'src/post.jpg',
+        hashtags: ['#omagad', '#lol']
+    },
+    {
+        id: '7',
+        author: 'Pablo228',
+        date: new Date(),
+        photoLink: 'src/post.jpg',
+        hashtags: ['#omagad', '#lol']
+    },
+    {
+        id: '8',
+        author: 'Pablo228',
+        date: new Date(),
+        photoLink: 'src/Hlib-Paper.jpg',
+        hashtags: ['#omagad', '#lol']
+    },
+    {
+        id: '9',
+        author: 'F1latovv',
+        date: new Date(),
+        photoLink: 'src/Nikita-Max.jpg',
+        hashtags: ['#omagad', '#lol', '#nice_roof']
+    }
+];
+let iife = (function () {
     posts.forEach((item) => {
         let toPost = createPost(item);
         document.querySelector('main').appendChild(toPost);
     });
 })();
+
+let getArray = () => posts;
+
+function getPhotoPosts(skip, top, filterConfig) {
+    skip = skip || 0;
+    top = top || 10;
+
+    
+}
 
 function createPost(post) {
     let section = document.createElement('section');
@@ -111,8 +120,45 @@ function createPost(post) {
     return section.firstElementChild;
 }
 
-function addPhotoPost(post) {
-    let toPost = createPost(post);
-    let main = document.querySelector('main');
-    main.appendChild(toPost);
+function getPhotoPost(id) {
+    let res = {};
+    posts.forEach((item) => {
+        if (item.id === id) {
+            res = item;
+        }
+    });
+    return res;
+}
+
+function validatePhotoPost(post) {
+    if (!post.hasOwnProperty('id')) {
+        return false;
+    }
+    if (!post.hasOwnProperty('author')) {
+        return false;
+    }
+    if (!post.hasOwnProperty('date')) {
+        return false;
+    }
+    if (!post.hasOwnProperty('photoLink')) {
+        return false;
+    }
+    if (!post.hasOwnProperty('hashtags')) {
+        return false;
+    }
+    return true;
+}
+
+function addPhotoPost(obj) {
+    if (!validatePhotoPost(obj)) {
+        return false;
+    }
+    posts.push(obj);
+    return true;
+}
+
+function removePhotoPost(id) {
+    let removeIndex = posts.map(function (item) {return item.id}).indexOf(id);
+    posts.splice(removeIndex, 1);
+    return true;
 }
